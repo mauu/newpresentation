942599b76bb300cebba23686c879fb3b9aae6a29
DESKTOP-02ED209\Mau <m.pirillo@reply.it>
2017-10-06 15:48:57 +0200

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ElementiComponent} from "./elementi/elementi.component";
-import {HomeComponent} from "./home/home.component";
-import {PosComponent} from "./pos/pos.component";
+import {ElementiComponent} from './elementi/elementi.component';
+import {HomeComponent} from './home/home.component';
+import {PosComponent} from './pos/pos.component';
 
 const routes: Routes = [
   {
